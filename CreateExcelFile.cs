99f5cebaab8a96a7829d6307a2a1a75cb969a42b
LearnerDotnet <41544507+LearnerDotnet@users.noreply.github.com>
2019-02-06 11:28:50 -0600

    Initial commit

---
+++ CreateExcelFile.cs
@@ -0,0 +1,260 @@
+﻿// #define INCLUDE_WEB_FUNCTIONS
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Data;
+using System.Reflection;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace ExportToExcel
+{
+    public class CreateExcelFile
+    {
+        const int DATE_FORMAT_ID = 1;
+
+        public static bool CreateExcelDocument<T>(List<T> list, string xlsxFilePath)
+        {
+            DataSet ds = new DataSet();
+            ds.Tables.Add(ListToDataTable(list));
+
+            return CreateExcelDocument(ds, xlsxFilePath);
+        }
+        #region HELPER_FUNCTIONS
+        public static DataTable ListToDataTable<T>(List<T> list)
+        {
+            DataTable dt = new DataTable();
+
+            foreach (PropertyInfo info in typeof(T).GetProperties())
+            {
+                dt.Columns.Add(new DataColumn(info.Name, GetNullableType(info.PropertyType)));
+            }
+            foreach (T t in list)
+            {
+                DataRow row = dt.NewRow();
+                foreach (PropertyInfo info in typeof(T).GetProperties())
+                {
+                    if (!IsNullableType(info.PropertyType))
+                        row[info.Name] = info.GetValue(t, null);
+                    else
+                        row[info.Name] = (info.GetValue(t, null) ?? DBNull.Value);
+                }
+                dt.Rows.Add(row);
+            }
+            return dt;
+        }
+        private static Type GetNullableType(Type t)
+        {
+            Type returnType = t;
+            if (t.IsGenericType && t.GetGenericTypeDefinition().Equals(typeof(Nullable<>)))
+            {
+                returnType = Nullable.GetUnderlyingType(t);
+            }
+            return returnType;
+        }
+        private static bool IsNullableType(Type type)
+        {
+            return (type == typeof(string) ||
+                    type.IsArray ||
+                    (type.IsGenericType &&
+                     type.GetGenericTypeDefinition().Equals(typeof(Nullable<>))));
+        }
+
+        public static bool CreateExcelDocument(DataTable dt, string xlsxFilePath)
+        {
+            DataSet ds = new DataSet();
+            ds.Tables.Add(dt);
+            bool result = CreateExcelDocument(ds, xlsxFilePath);
+            ds.Tables.Remove(dt);
+            return result;
+        }
+        #endregion
+        public static bool CreateExcelDocument(DataSet ds, string excelFilename)
+        {
+            try
+            {
+                using (SpreadsheetDocument document = SpreadsheetDocument.Create(excelFilename, SpreadsheetDocumentType.Workbook))
+                {
+                    WriteExcelFile(ds, document);
+                }
+                Trace.WriteLine("Successfully created: " + excelFilename);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine("Failed, exception thrown: " + ex.Message);
+                return false;
+            }
+        }
+
+
+        private static void WriteExcelFile(DataSet ds, SpreadsheetDocument spreadsheet)
+        {
+            spreadsheet.AddWorkbookPart();
+            spreadsheet.WorkbookPart.Workbook = new DocumentFormat.OpenXml.Spreadsheet.Workbook();
+            
+            spreadsheet.WorkbookPart.Workbook.Append(new BookViews(new WorkbookView()));
+            
+            WorkbookStylesPart workbookStylesPart = spreadsheet.WorkbookPart.AddNewPart<WorkbookStylesPart>("rIdStyles");
+            Stylesheet stylesheet = new Stylesheet();
+            workbookStylesPart.Stylesheet = stylesheet;
+
+            
+            uint worksheetNumber = 1;
+            Sheets sheets = spreadsheet.WorkbookPart.Workbook.AppendChild<Sheets>(new Sheets());
+            foreach (DataTable dt in ds.Tables)
+            {
+                string worksheetName = dt.TableName;
+                
+                WorksheetPart newWorksheetPart = spreadsheet.WorkbookPart.AddNewPart<WorksheetPart>();
+                Sheet sheet = new Sheet() { Id = spreadsheet.WorkbookPart.GetIdOfPart(newWorksheetPart), SheetId = worksheetNumber, Name = worksheetName };
+
+                sheets.Append(sheet);
+                
+                WriteDataTableToExcelWorksheet(dt, newWorksheetPart);
+
+                worksheetNumber++;
+            }
+
+            spreadsheet.WorkbookPart.Workbook.Save();
+        }
+
+        private static void WriteDataTableToExcelWorksheet(DataTable dt, WorksheetPart worksheetPart)
+        {
+            OpenXmlWriter writer = OpenXmlWriter.Create(worksheetPart, Encoding.ASCII);
+            writer.WriteStartElement(new Worksheet());
+            writer.WriteStartElement(new SheetData());
+
+            string cellValue = "";
+            int numberOfColumns = dt.Columns.Count;
+            bool[] IsNumericColumn = new bool[numberOfColumns];
+            bool[] IsDateColumn = new bool[numberOfColumns];
+
+            string[] excelColumnNames = new string[numberOfColumns];
+            for (int n = 0; n < numberOfColumns; n++)
+                excelColumnNames[n] = GetExcelColumnName(n);
+            
+            //
+            uint rowIndex = 1;
+
+            writer.WriteStartElement(new Row { RowIndex = rowIndex });
+            for (int colInx = 0; colInx < numberOfColumns; colInx++)
+            {
+                DataColumn col = dt.Columns[colInx];
+                AppendTextCell(excelColumnNames[colInx] + "1", col.ColumnName, ref writer);
+                IsNumericColumn[colInx] = (col.DataType.FullName == "System.Decimal") || (col.DataType.FullName == "System.Int32") || (col.DataType.FullName == "System.Double") || (col.DataType.FullName == "System.Single");
+                IsDateColumn[colInx] = (col.DataType.FullName == "System.DateTime");
+            }
+            writer.WriteEndElement();  
+            double cellNumericValue = 0;
+            foreach (DataRow dr in dt.Rows)
+            {
+                ++rowIndex;
+
+                writer.WriteStartElement(new Row { RowIndex = rowIndex });
+
+                for (int colInx = 0; colInx < numberOfColumns; colInx++)
+                {
+                    cellValue = dr.ItemArray[colInx].ToString();
+                    cellValue = ReplaceHexadecimalSymbols(cellValue);
+
+                    // Create cell with data
+                    if (IsNumericColumn[colInx])
+                    {
+                        cellNumericValue = 0;
+                        if (double.TryParse(cellValue, out cellNumericValue))
+                        {
+                            cellValue = cellNumericValue.ToString();
+                            AppendNumericCell(excelColumnNames[colInx] + rowIndex.ToString(), cellValue, ref writer);
+                        }
+                    }
+                    else if (IsDateColumn[colInx])
+                    {
+                        DateTime dtValue;
+                        string strValue = "";
+                        if (DateTime.TryParse(cellValue, out dtValue))
+                            strValue = dtValue.ToShortDateString();
+                        AppendTextCell(excelColumnNames[colInx] + rowIndex.ToString(), strValue, ref writer);
+                    }
+                    else
+                    {
+                        AppendTextCell(excelColumnNames[colInx] + rowIndex.ToString(), cellValue, ref writer);
+                    }
+                }
+                writer.WriteEndElement(); //  End of Row
+            }
+            writer.WriteEndElement(); //  End of SheetData
+            writer.WriteEndElement(); //  End of worksheet
+
+            writer.Close();
+        }
+
+        private static void AppendTextCell(string cellReference, string cellStringValue, ref OpenXmlWriter writer)
+        {
+            writer.WriteElement(new Cell 
+            { 
+                CellValue = new CellValue(cellStringValue), 
+                CellReference = cellReference, 
+                DataType = CellValues.String 
+            });
+        }
+
+        private static void AppendNumericCell(string cellReference, string cellStringValue, ref OpenXmlWriter writer)
+        {
+            writer.WriteElement(new Cell 
+            { 
+                CellValue = new CellValue(cellStringValue), 
+                CellReference = cellReference, 
+                DataType = CellValues.Number 
+            });
+        }
+
+        private static string ReplaceHexadecimalSymbols(string txt)
+        {
+            string r = "[\x00-\x08\x0B\x0C\x0E-\x1F\x26]";
+            return Regex.Replace(txt, r, "", RegexOptions.Compiled);
+        }
+        
+        public static string GetExcelColumnName(int columnIndex)
+        {
+            char firstChar;
+            char secondChar;
+            char thirdChar;
+
+            if (columnIndex < 26)
+            {
+                return ((char)('A' + columnIndex)).ToString();
+            }
+
+            if (columnIndex < 702)
+            {
+                firstChar = (char)('A' + (columnIndex / 26) - 1);
+                secondChar = (char)('A' + (columnIndex % 26));
+
+                return string.Format("{0}{1}", firstChar, secondChar);
+            }
+
+            int firstInt = columnIndex / 26 / 26;
+            int secondInt = (columnIndex - firstInt * 26 * 26) / 26;
+            if (secondInt == 0)
+            {
+                secondInt = 26;
+                firstInt = firstInt - 1;
+            }
+            int thirdInt = (columnIndex - firstInt * 26 * 26 - secondInt * 26);
+
+            firstChar = (char)('A' + firstInt - 1);
+            secondChar = (char)('A' + secondInt - 1);
+            thirdChar = (char)('A' + thirdInt);
+
+            return string.Format("{0}{1}{2}", firstChar, secondChar, thirdChar);
+        }
+
+    }
+}
